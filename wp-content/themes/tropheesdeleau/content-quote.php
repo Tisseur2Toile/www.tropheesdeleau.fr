9e7a4dac5a7de9b55168b98818bd2f421a90a4df
jeFFF <tisseur2toile@gmail.com>
2015-12-01 12:58:13 +0100

    Seance 1/12/2015

--- wp-content/themes/tropheesdeleau/content-quote.php
+++ wp-content/themes/tropheesdeleau/content-quote.php
@@ -11,12 +11,11 @@
 
 ?>
 <article>
-	<h2>Catégorie</h2>
-	<h1 id="article_important">Lorem ipsum dolor sit amet</h1>
-	<img src="http://placehold.it/555x155">
-	<p>
-		Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker. <a href="#">Lire la suite</a>
-	</p>
+	<h2><?php the_category(); ?></h2>
+	<h1 id="article_important"><?php the_title(); ?></h1>
+	<?php the_post_thumbnail('accueil'); ?>
+	<?php the_excerpt(); ?>
+	<a href="<?php the_permalink(); ?>">Lire la suite...</a>
 	<ul class="social_media">
 		<li><img src="http://placehold.it/18x18"></li>
 		<li><img src="http://placehold.it/18x18"></li>
